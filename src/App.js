708fabccf3b0125ded12f5dd6954f340e2c4c9bc
Sanjay Purswani <sanjsanj@hotmail.com>
2019-02-10 20:30:33 +0000

    Trial firebase auth

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyBgKK4j2ZyI4ukxJUbXybX0XR-WqIYe-vc",
+  authDomain: "gatsby-firebase-b23a5.firebaseapp.com",
+  // ...
+};
+firebase.initializeApp(config);
+
+class SignInScreen extends React.Component {
+  // The component's Local state.
+  state = {
+    isSignedIn: false, // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: "popup",
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false,
+    },
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      this.setState({ isSignedIn: !!user });
+    });
+  }
+
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <h1>My App</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>
+          Welcome {firebase.auth().currentUser.displayName}! You are now
+          signed-in!
+        </p>
+        <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SignInScreen />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
